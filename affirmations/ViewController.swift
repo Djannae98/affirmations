1c2bcb461e22ff101270ac28c39eb6d92996a318
Djannae98 <72421098+Djannae98@users.noreply.github.com>
2020-11-30 20:11:42 -0600

    affirmation app

--- affirmations/ViewController.swift
+++ affirmations/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var affirmationsLabel: UILabel!
+    @IBOutlet weak var affirmationsTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func affirmButton(_ sender: Any) {
+        
+        let labelsText = affirmationsLabel.text!
+        let textField = affirmationsTextField.text!
+        
+        
+        affirmationsLabel.text = labelsText + textField + "/n"
+    }
+    
 }
 
